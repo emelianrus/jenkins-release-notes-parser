28bb799ba249ecff812f10ac7de1e4efaf9368c4
ruslan <ruslan.emelianov@yahoo.com>
2023-03-18 22:41:32 +0200

    fix github module

--- github/github.go
+++ github/github.go
@@ -4,81 +4,73 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/emelianrus/jenkins-release-notes-parser/types"
 )
 
-func GetGitHubReleases(pluginName string) ([]types.GitHubReleaseNote, error) {
-	fmt.Println("Downloading plugin from github " + pluginName)
-	// Define cron job to run every hour
-	// c := cron.New()
-	// c.AddFunc("@hourly", func() {
-	// log.Println("Fetching release notes from GitHub API...")
+func Download(pluginName string) ([]types.GitHubReleaseNote, error) {
+	fmt.Println("executed download goroutine " + pluginName)
+	client := http.Client{}
 
-	// Make request to GitHub API
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "https://api.github.com/repos/jenkinsci/"+pluginName+"/releases", nil)
+	// Make a request to the API to get the release notes
+	url := fmt.Sprintf("https://api.github.com/repos/jenkinsci/%s/releases", pluginName)
+	fmt.Println(url)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		// log.Printf("error in github request: %s\n", err)
-		return nil, fmt.Errorf("error in github request: %s", err)
+		fmt.Println("Error creating request:", err)
+		return nil, nil
 	}
-	req.Header.Set("Accept", "application/vnd.github.v3+json")
 
-	resp, err := client.Do(req)
-	if err != nil {
-		// log.Println(err)
-		return nil, fmt.Errorf("error during making client.Do request: %s", err)
+	// Loop until we get a successful response or hit the rate limit
+	for {
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println("Error making request:", err)
+			continue // Try again
+		}
+
+		if resp.StatusCode == http.StatusForbidden {
+			// We hit the rate limit, so wait for the X-RateLimit-Reset header and try again
+			fmt.Println("Hit rate limit, waiting...")
+
+			resetTimestampStr := resp.Header.Get("X-RateLimit-Reset")
+			resetTimestampInt, err := strconv.ParseInt(resetTimestampStr, 10, 64)
+
+			resetTime := time.Unix(resetTimestampInt, 0).UTC()
+			if err != nil {
+				panic(err)
+			}
+			nowEpoch := time.Now().Unix()
+			timeToWait := resetTimestampInt - nowEpoch
+
+			waitInt := int(timeToWait)
+			fmt.Printf("Rate limit reached, waiting until %d seconds, until %s...\n", waitInt, resetTime)
+			time.Sleep(time.Second * time.Duration(waitInt))
+
+			continue // Try again
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			// The API returned an error, so print the status code and message and exit
+			fmt.Printf("API error: %s - %s", resp.Status, resp.Body)
+			return nil, nil
+		}
+		var releases []types.GitHubReleaseNote
+		err = json.NewDecoder(resp.Body).Decode(&releases)
+		if err != nil {
+			// fmt.Println("error decoding github response")
+			// log.Println(err)
+			return nil, nil
+		}
+
+		// The request was successful, so print the release notes and exit
+		// fmt.Println("Release notes:")
+		// fmt.Println(releases)
+
+		fmt.Println("finished download goroutine " + pluginName)
+
+		return releases, nil
 	}
-	defer resp.Body.Close()
-
-	// Check rate limit remaining
-	// rateLimitRemaining, err := strconv.Atoi(resp.Header.Get("X-Ratelimit-Remaining"))
-	// if err != nil {
-	// 	log.Println(err)
-	// 	return
-	// }
-
-	// If rate limit is reached, wait for 1 hour
-	// if rateLimitRemaining <= 0 {
-	// 	log.Println("Rate limit reached. Waiting 1 hour...")
-	// 	return
-	// }
-
-	// Decode response body into []GitHubReleaseNote
-	var releases []types.GitHubReleaseNote
-	err = json.NewDecoder(resp.Body).Decode(&releases)
-	if err != nil {
-		// fmt.Println("error decoding github response")
-		// log.Println(err)
-		return nil, fmt.Errorf("error decoding github response: %s", err)
-	}
-	return releases, nil
-	// ownerName := "jenkinsci"
-	// repoName := "plugin-installation-manager-tool"
-	// // Cache releases in Redis for 1 hour
-
-	// jsonData, err := json.Marshal(releases)
-	// if err != nil {
-	// 	log.Println(err)
-	// 	return
-	// }
-
-	// err = redisclient.Set(fmt.Sprintf("github:%s:%s:%s", "jenkinsci", pluginName, "lastUpdated"),
-	// 	time.Now())
-	// if err != nil {
-	// 	log.Println(err)
-	// 	return
-	// }
-
-	// log.Println("Releases cached in Redis")
-
-	// tmpl := template.Must(template.ParseFiles("templates/release-notes.html"))
-
-	// Start cron job
-	// c.Start()
-	// c.Run()
-	// Set up HTTP server
-	// StartWeb(redisclient)
-	// return releases
-	// return nil
 }
---
+++ github/queue.go
@@ -0,0 +1,30 @@
+package github
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/emelianrus/jenkins-release-notes-parser/db"
+)
+
+var (
+	serviceMutex sync.Mutex
+)
+
+// used as go StartQueue()
+// can be executed by button from UI so we need to be sure running only one instance at once
+func StartQueue(redisclient *db.Redis) {
+	serviceMutex.Lock()
+	defer serviceMutex.Unlock()
+
+	for {
+		for _, pluginName := range redisclient.GetAllPluginsFromServers() {
+			// TODO: error api 404
+			ghReleaseNotes, _ := Download(pluginName)
+			redisclient.SaveReleaseNotesToDB(ghReleaseNotes, pluginName)
+		}
+		fmt.Println("sleep 3 hours")
+		time.Sleep(time.Hour * 3)
+	}
+}
