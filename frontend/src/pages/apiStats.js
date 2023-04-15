6e230a0b595e69e0692585ca6f64152f75f3ec01
ruslan <ruslan.emelianov@yahoo.com>
2023-04-15 15:39:15 +0300

    fix js class name

--- frontend/src/pages/apiStats.js
+++ frontend/src/pages/apiStats.js
@@ -7,38 +7,38 @@ function APIStats() {
 
 
   return (
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md">
-          <div class="stats-container">
-            <div class="stats-column">
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md">
+          <div className="stats-container">
+            <div className="stats-column">
               <h2>github stats</h2>
-              <div class="stats-row">
-                <div class="stats-key">RateLimit</div>
-                <div class="stats-value">11</div>
+              <div className="stats-row">
+                <div className="stats-key">RateLimit</div>
+                <div className="stats-value">11</div>
               </div>
-              <div class="stats-row">
-                <div class="stats-key">RateLimitRemaning</div>
-                <div class="stats-value">11</div>
+              <div className="stats-row">
+                <div className="stats-key">RateLimitRemaning</div>
+                <div className="stats-value">11</div>
               </div>
-              <div class="stats-row">
-                <div class="stats-key">RateLimitReset</div>
-                <div class="stats-value">1234</div>
+              <div className="stats-row">
+                <div className="stats-key">RateLimitReset</div>
+                <div className="stats-value">1234</div>
               </div>
-              <div class="stats-row">
-                <div class="stats-key">RateLimitUsed</div>
-                <div class="stats-value">1234</div>
+              <div className="stats-row">
+                <div className="stats-key">RateLimitUsed</div>
+                <div className="stats-value">1234</div>
               </div>
-              <div class="stats-row">
-                <div class="stats-key">WaitSlotSeconds</div>
-                <div class="stats-value">1234</div>
+              <div className="stats-row">
+                <div className="stats-key">WaitSlotSeconds</div>
+                <div className="stats-value">1234</div>
               </div>
             </div>
-            <div class="stats-column">
+            <div className="stats-column">
               <h2>plugin center stats</h2>
-              <div class="stats-row">
-                <div class="stats-key">last downloaded</div>
-                <div class="stats-value">123</div>
+              <div className="stats-row">
+                <div className="stats-key">last downloaded</div>
+                <div className="stats-value">123</div>
               </div>
             </div>
           </div>
